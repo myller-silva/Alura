495890cf3c2705419da3bc52d9c854a27c50250b
Myller Silva <myllersilva1310@gmail.com>
2022-02-22 09:19:15 -0300

    update

--- React/alura-styled-components-master/src/Components/Conta/index.jsx
+++ React/alura-styled-components-master/src/Components/Conta/index.jsx
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
+import { Box, Button, Detalhe, Icone, Saldo } from "../UI";
+import styled from "styled-components";
+
+const IconeMargin = styled(Icone)`
+  marginTop: 2px; 
+`
 
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
@@ -12,29 +18,27 @@ const Conta = () => {
   };
 
   return (
-    <div className="box">
+    <Box>
       <h2>Conta</h2>
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponível{" "}
         <span>
-          <img className="imagem-icone" src={dinheiro} alt="Ícone Saldo" />
+          <Icone src={dinheiro} alt="Ícone Saldo" />
         </span>
         {toggleState ? (
-          <div className="saldo">
-            <span className="detalhe">R$</span> 0,00{" "}
-          </div>
+          <Saldo>
+            <Detalhe>R$</Detalhe> 0,00{" "}
+          </Saldo>
         ) : null}
       </div>
 
-      <button className="btn" onClick={toggleHandler}>
-        <img
-          style={{ marginTop: "2px" }}
-          className="imagem-icone"
+      <Button onClick={toggleHandler}>
+        <IconeMargin
           src={toggleState ? privado : olho_icone}
           alt="Privacidade do Saldo"
         />
-      </button>
-    </div>
+      </Button>
+    </Box>
   );
 };
 
--- React/alura-styled-components-master/src/Components/Container/index.jsx
+++ React/alura-styled-components-master/src/Components/Container/index.jsx
@@ -1,17 +1,32 @@
 import React from "react";
-
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import styled from "styled-components";
+import Extrato from "../Extrato";
 
-const Container = () => {
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+` 
+
+export default () => {
   return (
-    <div className="container">
+    <Container>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo className="conteudo">
         <Conta />
-      </section>
-    </div>
+        <Extrato/>
+      </Conteudo>
+    </Container>
   );
 };
-
-export default Container;
